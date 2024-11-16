4dc845a7bc28d0317545273f67725766842d3b7e
physcrowley <53307335+physcrowley@users.noreply.github.com>
2024-11-16 01:17:01 -0500

    mise à jour du code de démarrage

--- DriverTest.java
+++ DriverTest.java
@@ -7,39 +7,41 @@ import java.io.*;
 
 public class DriverTest {
 
+    /** Objet qui contient les méthodes à tester */
+    Driver testClass;
+
     @Test
     public void testIsValidQuizDataFile() {
 
+        testClass = new Driver();
 
-        Driver testClass = new Driver();
-        File quizTestFolder = new File("./test_data");
-        
+        /*
+         * Enlever les commentaires pour ajouter les cas de tests.
+         * 
+         * Les cas de test sont déclarés dans l'ordre logique pour la 
+         * structure valide de fichier pour un quiz.
+         * 
+         * Le premier test, pour un fichier vide, est actif (décommenté).
+         */
         HashMap<String, Boolean> expected = new HashMap<>();
-        expected.put("bad_index.qz", false);
         expected.put("empty.qz", false);
-        expected.put("missing_options.qz", false);
-        expected.put("not_a_number.qz", false);
-        expected.put("valid.qz", true);
-        expected.put("wrong_number.qz", false);
-        expected.put("wrong_order.qz", false);
-        expected.put("zero.qz", false);
-        
-        
-        File[] testFiles = quizTestFolder.listFiles();
+        // expected.put("zero.qz", false);
+        // expected.put("missing_options.qz", false);
+        // expected.put("wrong_order.qz", false);
+        // expected.put("not_a_number.qz", false);
+        // expected.put("bad_index.qz", false);
+        // expected.put("wrong_number.qz", false);
+        // expected.put("valid.qz", true);
 
-        // vérifier que `expected` a été bien définie
-        for (File f : testFiles) {
-            if (!expected.containsKey(f.getName())) {
-                System.out.print(f.getName());
-                System.out.println(" - Le test est mal formé. Revoir la définition des valeurs attendues");
-                return;
-            }
-        }
+        // liste de fichiers correspondant aux cas de test définis
+        File folder = new File("./test_data/");
+        File[] files = folder.listFiles(f -> expected.containsKey(f.getName()));
 
         // tests
-        for (File f : testFiles) {
+        for (File f : files) {
             String fileName = f.getName();
-            assertEquals(expected.get(fileName), testClass.isValidQuizDataFile(f));
+            String msg = "Test pour " + fileName + " : échoué";
+            assertEquals(msg, expected.get(fileName), testClass.isValidQuizDataFile(f));
         }
 
     }
@@ -47,7 +49,9 @@ public class DriverTest {
     @Test
     public void testFileStructure() {
 
-        Driver testClass = new Driver();
+        testClass = new Driver();
+
+        // cas de test
 
         String structureA = "./test_subfolderA";
         String structureB = "./test_subfolderB";
@@ -58,31 +62,40 @@ public class DriverTest {
                 "test_subfolderA(a(b(\"xx.txt\",\"yy.txt\",),\"x.txt\",),c(\"z.txt\",\"zzz.txt\",),),");
         expected.put(
                 structureB,
-                "test_subfolderB(.vscode(\"settings.json\",),bin(edu(ics4u(algos(\"App.class\",),),),),lib(\"hamcrest-core-1.3.jar\",\"junit-4.13.2.jar\",),src(edu(ics4u(algos(\"App.java\",),),),),\"README.md\",),");
-        
-        for (String root : new String[]{structureA, structureB})  {
+                "test_subfolderB(.vscode(\"settings.json\",),bin(edu(ics4u(algos(\"App.classy\",),),),),lib(\"hamcrest-core-1.3.jar\",\"junit-4.13.2.jar\",),\"README.md\",src(edu(ics4u(algos(\"App.coffee\",),),),),),");
+
+        // tests
+        for (String root : new String[] { structureA, structureB }) {
             File rootFile = new File(root);
             assertEquals(expected.get(root), testClass.fileStructure(rootFile));
-        }      
+        }
 
     }
 
     @Test
     public void testPrettyFileStructure() {
 
-        Driver testClass = new Driver();
+        testClass = new Driver();
+
+        // cas de test
 
         HashMap<String, String> expected = new HashMap<>();
+
         expected.put(
                 "test_subfolderA(a(b(\"xx.txt\",\"yy.txt\",),\"x.txt\",),c(\"z.txt\",\"zzz.txt\",),),",
-                "test_subfolderA\n    a\n        b\n            \"xx.txt\"\n            \"yy.txt\"\n        \"x.txt\"\n    c\n        \"z.txt\"\n        \"zzz.txt\"\n");
-        expected.put(
-                "test_subfolderB(.vscode(\"settings.json\",),bin(edu(ics4u(algos(\"App.class\",),),),),lib(\"hamcrest-core-1.3.jar\",\"junit-4.13.2.jar\",),src(edu(ics4u(algos(\"App.java\",),),),),\"README.md\",),",
-                "test_subfolderB\n    .vscode\n        \"settings.json\"\n    bin\n        edu\n            ics4u\n                algos\n                    \"App.class\"\n    lib\n        \"hamcrest-core-1.3.jar\"\n        \"junit-4.13.2.jar\"\n    \"README.md\"\n    src\n        edu\n            ics4u\n                algos\n                    \"App.java\"");
 
+                "test_subfolderA\n    a\n        b\n            \"xx.txt\"\n            \"yy.txt\"\n        \"x.txt\"\n    c\n        \"z.txt\"\n        \"zzz.txt\"");
+
+        expected.put(
+                "test_subfolderB(.vscode(\"settings.json\",),bin(edu(ics4u(algos(\"App.classy\",),),),),lib(\"hamcrest-core-1.3.jar\",\"junit-4.13.2.jar\",),\"README.md\",src(edu(ics4u(algos(\"App.coffee\",),),),),),",
+
+                "test_subfolderB\n    .vscode\n        \"settings.json\"\n    bin\n        edu\n            ics4u\n                algos\n                    \"App.classy\"\n    lib\n        \"hamcrest-core-1.3.jar\"\n        \"junit-4.13.2.jar\"\n    \"README.md\"\n    src\n        edu\n            ics4u\n                algos\n                    \"App.coffee\"");
+
+        // tests
         for (String fsOutput : expected.keySet()) {
             assertEquals(expected.get(fsOutput), testClass.prettyFileStructure(fsOutput));
         }
 
     }
+
 }
